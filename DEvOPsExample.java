2526ef1f512dab2b34834f77aed3da5e8593fb42
2020wa86372-Akshay <raj2akshay30612@gmail.com>
2024-06-07 15:56:02 +0530

    Create DevOpsExample.java

---
+++ DEvOPsExample.java
@@ -0,0 +1,8 @@
+/**
+*The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+*/
+public class DeopsExample {
+  public static void main(string[] args) {
+    system.out.println("Welcome to Devops.");
+  }
+}
